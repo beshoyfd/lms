dd1ddee646d0a393690d5b8f5a21dd5ad83d801e
Mohamed Mahfouz <moa.mahfouz@gmail.com>
2024-08-31 20:10:46 +0300

    updates to the subscribe section

--- resources/views/frontend/infixlmstheme/pages/index.blade.php
+++ resources/views/frontend/infixlmstheme/pages/index.blade.php
@@ -196,48 +196,64 @@
             <div class="col text-center">
                 <div class="ratio ratio-1x1 position-relative mx-auto mb-3 mb-sm-4" style="width: 68px;">
                     <i class="ai-search-settings text-primary fs-1 d-flex align-items-center justify-content-center position-absolute start-0"></i>
-                    <svg class="position-absolute top-0 start-0 text-primary" width="68" height="68" viewBox="0 0 68 68" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M56.0059 60.5579C44.1549 78.9787 18.0053 58.9081 6.41191 46.5701C-2.92817 35.5074 -2.81987 12.1818 11.7792 3.74605C30.0281 -6.79858 48.0623 7.40439 59.8703 15.7971C71.6784 24.1897 70.8197 37.5319 56.0059 60.5579Z" fill-opacity="0.1"></path>
+                    <svg class="position-absolute top-0 start-0 text-primary" width="68" height="68" viewBox="0 0 68 68"
+                         fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                        <path
+                            d="M56.0059 60.5579C44.1549 78.9787 18.0053 58.9081 6.41191 46.5701C-2.92817 35.5074 -2.81987 12.1818 11.7792 3.74605C30.0281 -6.79858 48.0623 7.40439 59.8703 15.7971C71.6784 24.1897 70.8197 37.5319 56.0059 60.5579Z"
+                            fill-opacity="0.1"></path>
                     </svg>
                 </div>
                 <h3 class="h4 pb-2 mb-1">Online support</h3>
-                <p class="fs-sm mb-0">Pharetra morbi quis is massa maecenas vulputate elit non nullage a duis tortor mi massa pharetra.</p>
+                <p class="fs-sm mb-0">Pharetra morbi quis is massa maecenas vulputate elit non nullage a duis tortor mi
+                    massa pharetra.</p>
             </div>
 
             <!-- Item -->
             <div class="col text-center">
                 <div class="ratio ratio-1x1 position-relative mx-auto mb-3 mb-sm-4" style="width: 68px;">
                     <i class="ai-bulb-alt text-primary fs-1 d-flex align-items-center justify-content-center position-absolute start-0"></i>
-                    <svg class="position-absolute top-0 start-0 text-primary" width="68" height="68" viewBox="0 0 68 68" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M65.0556 29.2672C75.4219 46.3175 48.5577 59.7388 33.8299 64.3181C21.0447 67.5599 1.98006 58.174 0.888673 42.8524C-0.475555 23.7004 18.3473 14.5883 29.9289 8.26059C41.5104 1.93285 52.0978 7.9543 65.0556 29.2672Z" fill-opacity="0.1"></path>
+                    <svg class="position-absolute top-0 start-0 text-primary" width="68" height="68" viewBox="0 0 68 68"
+                         fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                        <path
+                            d="M65.0556 29.2672C75.4219 46.3175 48.5577 59.7388 33.8299 64.3181C21.0447 67.5599 1.98006 58.174 0.888673 42.8524C-0.475555 23.7004 18.3473 14.5883 29.9289 8.26059C41.5104 1.93285 52.0978 7.9543 65.0556 29.2672Z"
+                            fill-opacity="0.1"></path>
                     </svg>
                 </div>
                 <h3 class="h4 pb-2 mb-1">100% guarantee</h3>
-                <p class="fs-sm mb-0">Sapien ultrices egestas at faucibus eu diam velit diam id amet nibh quam rutrum diam  diam natoque scelerisque.</p>
+                <p class="fs-sm mb-0">Sapien ultrices egestas at faucibus eu diam velit diam id amet nibh quam rutrum
+                    diam diam natoque scelerisque.</p>
             </div>
 
             <!-- Item -->
             <div class="col text-center">
                 <div class="ratio ratio-1x1 position-relative mx-auto mb-3 mb-sm-4" style="width: 68px;">
                     <i class="ai-circle-check-filled text-primary fs-2 d-flex align-items-center justify-content-center position-absolute start-0"></i>
-                    <svg class="position-absolute top-0 start-0 text-primary" width="68" height="68" viewBox="0 0 68 68" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M6.61057 18.2783C10.8205 -1.22686 39.549 7.51899 53.3869 14.3301C64.8949 20.7749 72.2705 40.7038 62.5199 52.5725C50.3318 67.4085 30.4034 61.0689 17.6454 57.6914C4.88745 54.314 1.3482 42.6597 6.61057 18.2783Z" fill-opacity="0.1"></path>
+                    <svg class="position-absolute top-0 start-0 text-primary" width="68" height="68" viewBox="0 0 68 68"
+                         fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                        <path
+                            d="M6.61057 18.2783C10.8205 -1.22686 39.549 7.51899 53.3869 14.3301C64.8949 20.7749 72.2705 40.7038 62.5199 52.5725C50.3318 67.4085 30.4034 61.0689 17.6454 57.6914C4.88745 54.314 1.3482 42.6597 6.61057 18.2783Z"
+                            fill-opacity="0.1"></path>
                     </svg>
                 </div>
                 <h3 class="h4 pb-2 mb-1">Work on time</h3>
-                <p class="fs-sm mb-0">Morbi et massa fames ac scelerisque sit commodo dignissim faucibus vel quisque proin lectus orbi et massa fames.</p>
+                <p class="fs-sm mb-0">Morbi et massa fames ac scelerisque sit commodo dignissim faucibus vel quisque
+                    proin lectus orbi et massa fames.</p>
             </div>
 
             <!-- Item -->
             <div class="col text-center">
                 <div class="ratio ratio-1x1 position-relative mx-auto mb-3 mb-sm-4" style="width: 68px;">
                     <i class="ai-rocket text-primary fs-2 d-flex align-items-center justify-content-center position-absolute start-0"></i>
-                    <svg class="position-absolute top-0 start-0 text-primary" width="68" height="68" viewBox="0 0 68 68" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M9.34481 53.5078C-7.24653 42.4218 11.4487 18.9206 22.8702 8.55583C33.0946 0.223307 54.3393 0.690942 61.7922 14.1221C71.1082 30.9111 57.886 47.1131 50.0546 57.7358C42.2233 68.3586 30.084 67.3653 9.34481 53.5078Z" fill-opacity="0.1"></path>
+                    <svg class="position-absolute top-0 start-0 text-primary" width="68" height="68" viewBox="0 0 68 68"
+                         fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                        <path
+                            d="M9.34481 53.5078C-7.24653 42.4218 11.4487 18.9206 22.8702 8.55583C33.0946 0.223307 54.3393 0.690942 61.7922 14.1221C71.1082 30.9111 57.886 47.1131 50.0546 57.7358C42.2233 68.3586 30.084 67.3653 9.34481 53.5078Z"
+                            fill-opacity="0.1"></path>
                     </svg>
                 </div>
                 <h3 class="h4 pb-2 mb-1">Free consultation</h3>
-                <p class="fs-sm mb-0">Consectetur adipiscing elit proin volutpat mollis egestas nam luctus facilisis ultrices pellentesque volutpat ligula est.</p>
+                <p class="fs-sm mb-0">Consectetur adipiscing elit proin volutpat mollis egestas nam luctus facilisis
+                    ultrices pellentesque volutpat ligula est.</p>
             </div>
         </div>
     </section>
@@ -279,38 +295,7 @@
     <x-home-page-faq :homeContent="$homeContent"/>
 
 
-    <div class="container my-5">
-        <div class="card text-center shadow-sm border-0 subscribe-newsletter">
-            <div class="row no-gutters align-items-center">
-                <div class="col-md-4">
-                    <img src="{{asset('/public/frontend2/vendor/subscribe-img.webp')}}" class="card-img"
-                         alt="Newsletter Image">
-                </div>
-                <div class="col-md-8">
-                    <div class="card-body">
-                        <h5 class="card-title text-right">
-                            <strong>Subscribe</strong> in Bakkah newsletter and don't miss a thing with our offers, new
-                            courses & more!
-                        </h5>
-                        <form action="{{route('subscribe')}}"
-                              method="POST" class="subscription relative">@csrf
-                            <input name="email" class="form-control"
-                                   placeholder="{{__('frontend.Enter e-mail Address')}}"
-                                   onfocus="this.placeholder = ''"
-                                   onblur="this.placeholder = '{{__('frontend.Email')}}'"
-                                   required="" type="email" value="{{old('email')}}">
-                            <button class="btn btn-primary btn-sm w-100 mt-3">{{__('frontend.Subscribe Now')}}</button>
-                            <div class="info">
-                                @if(isset($errors) && $errors->any())
-                                    <span class="text-danger" role="alert">{{$errors->first('email')}}</span>
-                                @endif
-                            </div>
-                        </form>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+
 
     <!-- CTA -->
     <section class="bg-primary py-5" data-bs-theme="dark">
@@ -320,19 +305,33 @@
                     <h2 class="display-3">Ready to take your business to the next level?</h2>
                 </div>
                 <div class="col-md-6 col-lg-5 col-xl-4 offset-lg-1">
-                    <p class="text-body fs-xl pb-4 mb-2 mb-lg-3">Massa velitienes semper faucibus tristique id nibh elementum, id eu aliquamd diam mi tempus at laciniarty scelerisques augue at morbi. Arcu sit orcirs, risus mattissit laoreet.</p>
-                    <div class="input-group">
-                <span class="input-group-text text-body-secondary">
-                  <i class="ai-mail"></i>
-                </span>
-                        <input class="form-control" type="email" placeholder="Enter your email">
-                        <button class="btn btn-warning" type="button">Book a call</button>
-                    </div>
+                    <form action="{{route('subscribe')}}"
+                          method="POST">
+                        @csrf
+                        <p class="text-body fs-xl pb-4 mb-2 mb-lg-3">Massa velitienes semper faucibus tristique id nibh
+                            elementum, id eu aliquamd diam mi tempus at laciniarty scelerisques augue at morbi. Arcu sit
+                            orcirs, risus mattissit laoreet.</p>
+                        <div class="input-group">
+                            <span class="input-group-text text-body-secondary">
+                              <i class="ai-mail"></i>
+                            </span>
+                            <input class="form-control" placeholder="{{__('frontend.Enter e-mail Address')}}"
+                                   onfocus="this.placeholder = ''"
+                                   onblur="this.placeholder = '{{__('frontend.Email')}}'"
+                                   required="" type="email" value="{{old('email')}}">
+                            <button class="btn btn-warning" type="submit">{{__('frontend.Subscribe Now')}}</button>
+                        </div>
+                        @if(isset($errors) && $errors->any())
+                            <span class="text-danger" role="alert">{{$errors->first('email')}}</span>
+                        @endif
+                    </form>
                 </div>
             </div>
             <div class="d-none d-md-block text-center mt-n5">
-                <svg class="text-warning ms-lg-5" width="171" height="97" viewBox="0 0 171 97" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M169.319 54.333C162.404 55.9509 155.712 58.0764 149.09 60.6764L149.07 60.6761C148.967 60.7158 148.863 60.7554 148.76 60.7951C147.3 61.3811 148.325 63.4238 149.672 63.2067C154.548 62.4134 159.994 59.8725 164.87 59.0792C148.278 73.1339 129.684 89.2549 107.779 92.6402C85.6981 96.0539 65.5665 86.7839 56.8768 66.9865C70.9662 55.0671 79.2106 35.6614 79.0299 17.6457C78.9484 10.3157 76.1485 -3.36373 65.7068 1.21851C55.8557 5.53146 52.0466 22.5213 50.5736 31.7739C48.7364 43.2858 49.7593 55.5291 53.8643 66.2014C52.787 67.0812 51.6907 67.8989 50.5755 68.6546C40.6328 75.3851 27.1039 78.8929 16.4487 72.0362C2.91045 63.3259 1.93984 44.9485 1.56902 30.4091C1.54778 29.6265 0.359869 29.6092 0.360624 30.3915C0.322634 44.0809 0.835929 59.065 10.5664 69.6857C18.5722 78.4182 30.4315 79.7753 41.3346 75.9924C46.2437 74.2834 50.7739 71.7557 54.8581 68.6348C59.9738 80.2586 68.9965 89.6956 82.2735 93.7393C113.474 103.223 141.744 83.0494 164.903 63.697L161.901 71.0334C161.267 72.5887 163.76 73.2736 164.393 71.7389C165.986 67.8713 167.569 63.9933 169.152 60.1359C169.288 60.0247 169.695 58.6127 169.821 58.491C170.122 57.1161 169.152 60.1359 169.851 58.4169C170.189 57.6087 170.517 56.79 170.855 55.9818C171.248 54.9994 170.185 54.1192 169.319 54.333ZM54.3624 59.8578C51.4872 49.1623 51.6051 37.5841 54.2025 26.8039C55.5185 21.3369 57.4405 15.8066 60.1572 10.8541C61.2311 8.89354 62.5139 6.77134 64.2307 5.31421C69.4231 0.902277 74.3649 4.80357 75.8002 10.4446C80.5272 28.9489 70.1806 51.6898 55.8431 64.5114C55.2971 63.0109 54.793 61.4698 54.3624 59.8578Z"></path>
+                <svg class="text-warning ms-lg-5" width="171" height="97" viewBox="0 0 171 97" fill="currentColor"
+                     xmlns="http://www.w3.org/2000/svg">
+                    <path
+                        d="M169.319 54.333C162.404 55.9509 155.712 58.0764 149.09 60.6764L149.07 60.6761C148.967 60.7158 148.863 60.7554 148.76 60.7951C147.3 61.3811 148.325 63.4238 149.672 63.2067C154.548 62.4134 159.994 59.8725 164.87 59.0792C148.278 73.1339 129.684 89.2549 107.779 92.6402C85.6981 96.0539 65.5665 86.7839 56.8768 66.9865C70.9662 55.0671 79.2106 35.6614 79.0299 17.6457C78.9484 10.3157 76.1485 -3.36373 65.7068 1.21851C55.8557 5.53146 52.0466 22.5213 50.5736 31.7739C48.7364 43.2858 49.7593 55.5291 53.8643 66.2014C52.787 67.0812 51.6907 67.8989 50.5755 68.6546C40.6328 75.3851 27.1039 78.8929 16.4487 72.0362C2.91045 63.3259 1.93984 44.9485 1.56902 30.4091C1.54778 29.6265 0.359869 29.6092 0.360624 30.3915C0.322634 44.0809 0.835929 59.065 10.5664 69.6857C18.5722 78.4182 30.4315 79.7753 41.3346 75.9924C46.2437 74.2834 50.7739 71.7557 54.8581 68.6348C59.9738 80.2586 68.9965 89.6956 82.2735 93.7393C113.474 103.223 141.744 83.0494 164.903 63.697L161.901 71.0334C161.267 72.5887 163.76 73.2736 164.393 71.7389C165.986 67.8713 167.569 63.9933 169.152 60.1359C169.288 60.0247 169.695 58.6127 169.821 58.491C170.122 57.1161 169.152 60.1359 169.851 58.4169C170.189 57.6087 170.517 56.79 170.855 55.9818C171.248 54.9994 170.185 54.1192 169.319 54.333ZM54.3624 59.8578C51.4872 49.1623 51.6051 37.5841 54.2025 26.8039C55.5185 21.3369 57.4405 15.8066 60.1572 10.8541C61.2311 8.89354 62.5139 6.77134 64.2307 5.31421C69.4231 0.902277 74.3649 4.80357 75.8002 10.4446C80.5272 28.9489 70.1806 51.6898 55.8431 64.5114C55.2971 63.0109 54.793 61.4698 54.3624 59.8578Z"></path>
                 </svg>
             </div>
         </div>
