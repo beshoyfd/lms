5a97c23a11d6ee4a121f5c91fc1a0338ff08b776
Beshoy AbdElmaseh <beshoy.abdelmaseh.dev@gmail.com>
2024-09-02 21:40:03 +0300

    added multi lang
    Took 53 minutes

---
+++ Modules/FrontendMultiLang/Routes/api.php
@@ -0,0 +1,9 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+
+//Route::middleware('auth:api')->get('/frontendMultiLang', function (Request $request) {
+//    return $request->user();
+//});
